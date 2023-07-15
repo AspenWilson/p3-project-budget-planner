67cd95d6bea7c9a6933f1d8472f47d81a36bc40a
Aspen Wilson <aspen.schell@gmail.com>
2023-07-15 12:37:39 -0700

    Created seed data

---
+++ lib/db/seed.py
@@ -0,0 +1,45 @@
+import random
+from faker import Faker 
+from models import Category, Expense, Income, engine
+from sqlalchemy.orm import sessionmaker
+
+fake = Faker()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def generate_seed_data():
+    categories = ['Food', 'Transportation', 'Housing', 'Utilities', 'Entertainment']
+    
+    category_instances = []
+
+    for category_name in categories:
+        category = Category(
+            name=category_name, 
+            budget=random.randint(100, 1000)
+        )
+
+        session.add(category)
+        category_instances.append(category)
+
+    for _ in range(20):
+        expense = Expense(
+            name=fake.text(max_nb_chars=20), 
+            amount=random.uniform(5, 100),
+            category=random.choice(category_instances)
+        )
+
+        session.add(expense) 
+
+    for _ in range(5):
+        income = Income(
+            name=fake.job(), 
+            amount=random.uniform(500, 3000)
+        )
+        
+        session.add(income)
+    
+    session.commit()
+
+if __name__ == '__main__':
+    generate_seed_data()
+
