0f2de3a59cc92df26bf8679862ea927347011238
Aspen Wilson <aspen.schell@gmail.com>
2023-07-16 12:01:14 -0700

    Naming issues troubleshooting

--- lib/db/cli.py
+++ lib/db/cli.py
@@ -6,7 +6,7 @@ def welcome_message():
     print("Welcome to your Budget Planner CLI!")
     print("-----------------------------------")
     print("You have many options to choose from to help you set and maintain your budget:")
-    print("View_Budget: This shows you your current budgets, broken out by category.")
+    print("View: This shows you your current budgets, broken out by category.")
     print("Add_Expense: This is where you can add a new expense.")
     print("Add_Income: This is where you can add income.")
     print("Set_Budget: This is where you can set the budget for each category based on a percentage of your monthly income.")
@@ -17,26 +17,32 @@ def welcome_message():
 def cli():
     welcome_message()
 
+@cli.command(name='view')
 def view_budget():
     budget_cli = BudgetCLI()
     budget_cli.view_budget()
 
+@cli.command(name='add_expense')
 def add_expense():
     budget_cli = BudgetCLI()
     budget_cli.add_expense()
 
+@cli.command(name='add_income')
 def add_income():
     budget_cli = BudgetCLI()
     budget_cli.add_income()
 
+@cli.command(name='set_budget')
 def set_budget():
     budget_cli = BudgetCLI()
     budget_cli.set_budget()
 
+@cli.command(name='summary')
 def summary():
     budget_cli = BudgetCLI()
     budget_cli.summary()
 
+@cli.command(name='delete_expense')
 def delete_expense():
     budget_cli = BudgetCLI()
     budget_cli.delete_expense()
